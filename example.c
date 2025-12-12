2411b4b010acac7b609c49965f582b6268b7818c
Steven Reid <sreid1118@gmail.com>
2025-12-11 20:51:45 -0500

    Update and rename examples.c to example.c

--- example.c
+++ example.c
@@ -8,3 +8,4 @@ int main() {
     for (int i = 0; i < 10; i++)
         printf("%016llx\n", (unsigned long long)rdt_prng_next(&prng));
 }
+gcc -O3 rdt_core.c rdt_prng.c rdt_drbg.c example.c -o example
