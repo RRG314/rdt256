aaa42017371e408d11f29d48a67f40fb110d6bb2
Steven Reid <sreid1118@gmail.com>
2025-12-11 21:42:40 -0500

    Update rdt.h

--- src/rdt.h
+++ src/rdt.h
@@ -2,36 +2,15 @@
 #define RDT_H
 
 #include <stdint.h>
-#include <stddef.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+uint64_t rdt_mix(uint64_t x, uint64_t K[4]);
 
-/* ---------------- Core Mixing Primitive ---------------- */
-uint64_t rdt_mix(uint64_t x, const uint64_t K[4]);
+/* PRNG */
+void rdt_prng_init(uint64_t seed);
+uint64_t rdt_prng_next(void);
 
-/* ---------------- Simple PRNG (non-cryptographic) ---------------- */
-typedef struct {
-    uint64_t S[4];
-} rdt_prng_state;
-
-void rdt_prng_init(rdt_prng_state *st, uint64_t seed);
-uint64_t rdt_prng_next(rdt_prng_state *st);
-
-/* ---------------- DRBG (cryptographically structured) ---------------- */
-typedef struct {
-    uint64_t K[4];     /* key material */
-    uint64_t S[4];     /* internal state */
-    uint64_t reseed_counter;
-} rdt_drbg_state;
-
-void rdt_drbg_init(rdt_drbg_state *st, const uint64_t seed[4]);
-void rdt_drbg_reseed(rdt_drbg_state *st, const uint64_t seed[4]);
-uint64_t rdt_drbg_next(rdt_drbg_state *st);
-
-#ifdef __cplusplus
-}
-#endif
+/* DRBG */
+void rdt_drbg_init(uint64_t seed);
+uint64_t rdt_drbg_next(void);
 
 #endif
